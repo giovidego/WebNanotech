046177c8d00981fd523093bd4d7db93a06e1b586
unknown <giorgiovidelagonzalez@gmail.com>
2024-12-08 22:40:49 -0300

    v0.6

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import messages
 from .carro import Carro
 
@@ -13,19 +12,3 @@ def importe_total_carro(request):
             total += precio * cantidad
 
     return {'importe_total_carro': total}
-=======
-from django.contrib import messages
-from .carro import Carro
-
-
-def importe_total_carro(request):
-    total = 0
-    if 'carro' in request.session:
-        carro = request.session['carro']
-        for value in carro.values():
-            cantidad = int(value.get('cantidad'))
-            precio = int(value.get('precio'))
-            total += precio * cantidad
-
-    return {'importe_total_carro': total}
->>>>>>> 77b3203bc8f619758b59b020ded599290df4236a
